e1595555fb15b0b2a6c336884cdc7612c5488a48
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-10-09 05:33:46 +0530

    Update client.java and server.java to send and receive data from and to files

--- src/Client.java
+++ src/Client.java
@@ -1,44 +1,30 @@
 import java.io.DataOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Client {
     public static void main(String args[]) {
-
         try {
-            // Now we will create a socket object and connect it to the server
-            // Here localhost means that the server is running on the same machine as the
-            // client
-            // If the server is running on a different machine then we will have to enter
-            // the IP address of that machine instead of localhost
-            // 3000 is the port number on which the server is running
+
             Socket socket = new Socket("localhost", 3000);
             System.out.println("Connected to server at " + socket);
-            // Now we will create an object of DataOutputStream to send the message to the
-            // server
+
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            Scanner sc = new Scanner(System.in);
-            // Now we will ask the user to enter the message that he wants to send to the
-            // server
-            System.out.println("Enter the message that you want to send to the server:");
-            String message = sc.nextLine();
+            String fileContent = new String(Files.readAllBytes(Paths.get("fileToRead.txt")));
 
-            // Now we will send the message to the server
-            out.writeUTF(message);
+            System.out.println("File content: \n" + fileContent);
 
-            // Now we will close the connection
+            // send the file content to the server
+            out.writeUTF(fileContent);
+
             out.flush();
-            // flush method forces any buffered output bytes to be written out
-
             out.close();
+
             socket.close();
-            sc.close();
-        }
-        // If any exception occurs we will print it on the console
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,29 +7,25 @@ public class Server {
 
   public static void main(String args[]) {
     try {
-      // Instantiate a ServerSocket object and bind it to port 3000
-      // This will simply run the server on port 3000
       ServerSocket server = new ServerSocket(3000);
       System.out.println("Server started. Listening on port " + 3000 + "...");
 
-      // Now we will wait for the client to connect to our server
-      // The accept() method will block the execution until a client connects to the
-      // server
       Socket socket = server.accept();
       System.out.println("Client connected from: " + socket);
 
-      // Now we will create and object of DataInputStream to read the message sent by
-      // the client
       DataInputStream in = new DataInputStream(socket.getInputStream());
 
+      // receive the file content from the client
       String message = in.readUTF();
 
-      System.out.println("Received message: " + message);
+      FileOutputStream fileOutputStream = new FileOutputStream("receivedData.txt", true);
+      fileOutputStream.write(message.getBytes());
+      fileOutputStream.close();
 
-      // Now we will close the connection
+      System.out.println("Received data: \n" + message);
+
       server.close();
     } catch (Exception e) {
-      // If any exception occurs we will print it on the console
       System.out.println(e);
     }
   }
