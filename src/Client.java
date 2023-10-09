05e3281e8bb0097668c4c9427ba70c9f47935f49
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-10-09 05:40:18 +0530

    Update code formatting

--- src/Client.java
+++ src/Client.java
@@ -4,27 +4,27 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Client {
-    public static void main(String args[]) {
-        try {
+  public static void main(String args[]) {
+    try {
 
-            Socket socket = new Socket("localhost", 3000);
-            System.out.println("Connected to server at " + socket);
+      Socket socket = new Socket("localhost", 3000);
+      System.out.println("Connected to server at " + socket);
 
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+      DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            String fileContent = new String(Files.readAllBytes(Paths.get("fileToRead.txt")));
+      String fileContent = new String(Files.readAllBytes(Paths.get("fileToRead.txt")));
 
-            System.out.println("File content: \n" + fileContent);
+      System.out.println("File content: \n" + fileContent);
 
-            // send the file content to the server
-            out.writeUTF(fileContent);
+      // send the file content to the server
+      out.writeUTF(fileContent);
 
-            out.flush();
-            out.close();
+      out.flush();
+      out.close();
 
-            socket.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+      socket.close();
+    } catch (Exception e) {
+      System.out.println(e);
     }
+  }
 }
--- src/FileReadTry.java
+++ src/FileReadTry.java
@@ -5,32 +5,32 @@ import java.nio.file.Paths;
 import java.io.FileInputStream;
 
 public class FileReadTry {
-    // make a file and read the contents of a file named 'fileEx.md' using input
-    // stream of java
+  // make a file and read the contents of a file named 'fileEx.md' using input
+  // stream of java
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        File file = new File("rd.md");
+    File file = new File("rd.md");
 
-        System.out.println(String.format("Data from '%s':-", file));
+    System.out.println(String.format("Data from '%s':-", file));
 
-        try (InputStream in = new FileInputStream(file)) {
-            int content;
-            System.out.print("Data: \n");
-            while ((content = in.read()) != -1) {
-                System.out.print((char) content);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // alternative way to read the file
-        try {
-            String fileContent = new String(Files.readAllBytes(Paths.get("rd.md")));
-            System.out.println("File content: \n" + fileContent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    try (InputStream in = new FileInputStream(file)) {
+      int content;
+      System.out.print("Data: \n");
+      while ((content = in.read()) != -1) {
+        System.out.print((char) content);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+    // alternative way to read the file
+    try {
+      String fileContent = new String(Files.readAllBytes(Paths.get("rd.md")));
+      System.out.println("File content: \n" + fileContent);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
 }
